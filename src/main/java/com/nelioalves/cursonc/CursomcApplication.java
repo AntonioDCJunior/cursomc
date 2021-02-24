e547859b7662d5a28fdd1669f77ad0050aeb778d
Antonio Jr <antoniodiascjr@gmail.com>
2021-02-24 16:32:38 -0300

    Projeto criado

---
+++ src/main/java/com/nelioalves/cursonc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.nelioalves.cursonc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
